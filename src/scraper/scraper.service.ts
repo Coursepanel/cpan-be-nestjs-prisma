eb7295fc9b31b781a3c19bdeb5900238cab57490
ankit <ankit.sanghavi87@gmail.com>
2022-03-29 11:29:58 +0530

    new approach for getting data

--- src/scraper/scraper.service.ts
+++ src/scraper/scraper.service.ts
@@ -1,53 +1,21 @@
+import querystring from 'querystring';
+import axios from 'axios';
+import { load } from 'cheerio';
+import * as puppeteer from 'puppeteer';
+import qs from 'qs';
+import * as FormData from 'form-data';
+
 import { Injectable } from '@nestjs/common';
 import cheerio, { Element, Node } from 'cheerio';
-import * as puppeteer from 'puppeteer';
+const url = 'https://academic.iitm.ac.in/course_detail.php';
 @Injectable()
 export class ScraperService {
   async scrapeWeb() {
-    const browser = await puppeteer.launch({ headless: false });
-    const page = await browser.newPage();
-    await page.goto('https://academic.iitm.ac.in/course_detail.php');
-    try {
-      const pageContent = await page.content();
-      const $ = cheerio.load(pageContent);
-      //TODO : add type safety
-      const deptDropdown = $('select#department') as any;
-      const deptsArray = Array.from(deptDropdown.children());
-      // console.log(deptsArray);
-      const options = deptsArray
-        .slice(1, deptsArray.length - 1)
-        .map((option: Element) => option.attribs.value);
-      deptDropdown.val(options[0]);
-      // console.log(options[0]);
-      // await page.click(options[0]);
-    } catch (error) {}
-
-    // console.log(someEl)
-
-    // const [el] = await page.$x(
-    //   '/html/body/div[1]/div[2]/div[9]/div[4]/div[3]/div[1]/div[1]/div/div/div[2]/div[1]/div[1]/ul/li[1]/span/span/div/img',
-    // );
-    // const src = await el.getProperty('src');
-    // const imgURL = await src.jsonValue();
-
-    // const [el2] = await page.$x(
-    //   '/html/body/div[1]/div[2]/div[9]/div[4]/div[4]/div[1]/div/h1/span',
-    // );
-    // const txt = await el2.getProperty('textContent');
-    // const title = await txt.jsonValue();
-
-    // const [el3] = await page.$x(
-    //   '/html/body/div[1]/div[2]/div[9]/div[4]/div[4]/div[10]/div[1]/div/table/tbody/tr[2]/td[2]/span[1]',
-    // );
-    // const txt2 = await el3.getProperty('textContent');
-    // const price = await txt2.jsonValue();
-
-    // console.log({
-    //   imgURL,
-    //   title,
-    //   price,
-    // });
-
-    // browser.close();
+    const form = new FormData();
+    form.append('pid', 'course_details');
+    form.append('dept_code', 'MS');
+    form.append('course', '');
+    const res = await axios.post(url, form, { headers: form.getHeaders() });
+    console.log(res, 'res');
   }
 }
--- src/scripts/scraper.ts
+++ src/scripts/scraper.ts
@@ -1,19 +1,54 @@
-import cheerio from 'cheerio';
-import * as puppeteer from 'puppeteer';
+import axios from 'axios';
+import * as FormData from 'form-data';
 
-(async () => {
-  const browser = await puppeteer.launch({ headless: false });
-  const page = await browser.newPage();
-  await page.goto('https://academic.iitm.ac.in/course_detail.php');
-  try {
-    const pageContent = await page.content;
-    const $ = cheerio.load(await pageContent());
-    const deptDropdown = $('select#department');
-    //TODO : add type safety
-    const options = Array.from(deptDropdown.children as any).slice(1);
-    console.log(options);
-    deptDropdown.val((options[0] as HTMLOptionElement).label);
-    await page.click('#slot_view');
-    console.log('#slot_view was clicked');
-  } catch (error) {}
-})();
+const url = 'https://academic.iitm.ac.in/course_detail.php';
+const scrape = async () => {
+  const form = new FormData();
+  form.append('pid', 'course_details');
+  form.append('dept_code', 'MS');
+  form.append('course', '');
+  console.log(form.getHeaders());
+  const res = await axios.post(url, form, {
+    headers: {
+      ...form.getHeaders(),
+      'content-type': 'application/x-www-form-urlencoded',
+    },
+  });
+  console.log(res);
+};
+scrape();
+
+// ? LEGACY APPROACH  - actually hand-scraping each course's details
+// const puppeteerScrapeWeb = () => {
+//   // const browser = await puppeteer.launch({ headless: false });
+//     // const page = await browser.newPage();
+//     // await page.goto('https://academic.iitm.ac.in/course_detail.php');
+//   try {
+//     // const pageContent = await page.content();
+//     // const $ = load(pageContent);
+//     // //TODO : add type safety
+//     // const deptDropdown = $('select#department') as any;
+//     // const depts = deptDropdown.children();
+//     // Object.keys(depts).map(function (key, index) {
+//     //   if (index === 0) {
+//     //     depts[key].attribs.selected = '';
+//     //   }
+//     //   if (index === 1) {
+//     //     depts[key].attribs.selected = 'selected';
+//     //     deptDropdown.attr('value', depts[key].attribs.value);
+//     //     // deptDropdown.attribs.value = depts[key].attribs.value;
+//     //   }
+//     //   if (depts[key].name !== undefined)
+//     //     console.log(
+//     //       depts[key].name,
+//     //       depts[key].type,
+//     //       depts[key].attribs.selected,
+//     //       depts[key].attribs.value,
+//     //       index,
+//     //     );
+//     // });
+//     // await console.log(deptDropdown.value);
+//     // await page.click('#slot_view');
+//     // console.log(res);
+//   } catch (error) {}
+// }
