9ab45d87e241c10ed15adf6d1a3921d5729c978d
ankit <ankit.sanghavi87@gmail.com>
2022-04-20 21:15:05 +0530

    Merge branch 'master' of https://github.com/Fallow-Inc/courseplanner-backend

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: true,
   });
-  await app.listen(8080);
+  await app.listen(process.env.PORT);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
