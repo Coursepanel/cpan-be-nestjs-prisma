56d00995f76a0b6d75311b8498e8cae40de2e6ea
ankit <ankit.sanghavi87@gmail.com>
2022-04-25 04:02:20 +0530

    rate limiter working

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: {
-      origin: process.env.NODE_ENV !== 'production' ? '*' : /\.fallow\.in$/,
+      origin:
+        process.env.NODE_ENV !== 'production'
+          ? '*'
+          : [/\.fallow\.in$/, '103.158.43.20/32'],
       methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
       credentials: true,
     },
