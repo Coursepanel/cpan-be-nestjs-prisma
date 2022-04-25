5e67eca36aa5868366fc5443c5a9a3bd575982ef
ankit <ankit.sanghavi87@gmail.com>
2022-04-25 12:35:16 +0530

    temp cors disable

--- src/main.ts
+++ src/main.ts
@@ -4,16 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: {
-      origin:
-        process.env.NODE_ENV !== 'production'
-          ? '*'
-          : [
-              '^(https?://(?:.+.)?fallow.in(?::d{1,5})?)$',
-              'https://coursemap.fallow.in/*',
-              '103.158.43.20/32',
-            ],
+      origin: '*',
       methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
       credentials: true,
+      preflightContinue: false,
+      optionsSuccessStatus: 204,
     },
   });
   await app.listen(process.env.PORT);
