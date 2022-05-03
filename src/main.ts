b21cd125ce36dc175336829c3866ca8b20f9a268
ankit <ankit.sanghavi87@gmail.com>
2022-05-03 17:42:08 +0530

    port realloc

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(`Listening on ${process.env.PORT_LISTENING}`);
+  await app.listen(process.env.PORT);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
